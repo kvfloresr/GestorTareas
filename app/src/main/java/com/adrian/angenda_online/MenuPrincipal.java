6af6b7f5351d1941a984f1c5f62342b3dedf2fd0
kvfloresr <karenvfloresr@gmail.com>
2023-06-17 00:45:05 -0400

    listo

--- app/src/main/java/com/adrian/angenda_online/MenuPrincipal.java
+++ app/src/main/java/com/adrian/angenda_online/MenuPrincipal.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MenuPrincipal extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigationView);
+        // Configurar listener para gestionar los eventos de selección de elementos de la barra de navegación
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            // Aquí puedes manejar los eventos de selección de elementos de la barra de navegación
+            // y realizar acciones correspondientes según el elemento seleccionado
+            return true;
+        });
     }
 }
