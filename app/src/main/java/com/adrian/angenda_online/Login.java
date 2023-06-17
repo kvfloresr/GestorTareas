3160016652ba8f4ff2496a6dd5c1a6e775be4600
kvfloresr <karenvfloresr@gmail.com>
2023-06-17 03:36:47 -0400

    3:36 am

--- app/src/main/java/com/adrian/angenda_online/Login.java
+++ app/src/main/java/com/adrian/angenda_online/Login.java
@@ -2,7 +2,10 @@ package com.adrian.angenda_online;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button buttonIniciarSesion = findViewById(R.id.IniciarSesion);
+        buttonIniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Iniciar la actividad del menú principal
+                Intent intent = new Intent(Login.this, MenuPrincipal.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/adrian/angenda_online/MenuPrincipal.java
+++ app/src/main/java/com/adrian/angenda_online/MenuPrincipal.java
@@ -2,7 +2,9 @@ package com.adrian.angenda_online;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -14,11 +16,19 @@ public class MenuPrincipal extends AppCompatActivity {
         setContentView(R.layout.activity_menu_principal);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.navigationView);
-        // Configurar listener para gestionar los eventos de selección de elementos de la barra de navegación
-        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
-            // Aquí puedes manejar los eventos de selección de elementos de la barra de navegación
-            // y realizar acciones correspondientes según el elemento seleccionado
-            return true;
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                int itemId = item.getItemId();
+                if (itemId == R.id.navigation_artists) {
+                    // Iniciar la actividad de añadir
+                    Intent intent = new Intent(MenuPrincipal.this, Anadir.class);
+                    startActivity(intent);
+                    return true;
+                }
+                // Agrega más casos para otros elementos del menú si es necesario
+                return false;
+            }
         });
     }
 }
